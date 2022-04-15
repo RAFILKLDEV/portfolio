bc8b60ca73b44dafce6b52930d46749d6f92c23b
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-04-15 13:40:40 -0300

    fix: Build

---
+++ src/components/start/Start.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Start = () => {
+    return(
+        <div className="content">
+        <div style={{color: "rgb(255, 255, 42)"}}>Desenvolvedor Front-End Junior</div>
+        <div style={{border: "1px solid #fff", width:"50%"}}></div>
+        <div>Procurando uma Oportunidade!</div>
+      </div>
+    )
+}
+
+export default Start
